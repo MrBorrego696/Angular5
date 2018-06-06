e731512ab9d3360e35eaabe99c006a3194ba02ba
AlanIslas <alanislas696@gmail.com>
2018-06-06 15:46:48 -0500

    FINE

---
+++ src/Ejercicios/ejercicio1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'eje1',
+    template: `<h1>Aqui esta el pedo</h1>`
+})
+
+export class Ejercicio1{
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent, JokeComponent, JokeListComponent, JokeFormComponent } from './app.component';
 import { bootstrap } from '../../node_modules/bootstrap';
+import { Ejercicio1 } from '../Ejercicios/ejercicio1.component';
 
 @NgModule({
   declarations: [AppComponent,
                 JokeComponent,
                 JokeListComponent,
-                JokeFormComponent],
+                JokeFormComponent,
+                Ejercicio1],
   imports: [BrowserModule],
-  providers: [AppModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+              JokeComponent,
+              JokeListComponent,
+              JokeFormComponent,
+              Ejercicio1]
 })
+
 
 export class AppModule { }
